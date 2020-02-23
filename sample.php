22f7b67eb8ed1e36dbed7510c1104bc5abc745d7
ynj0906 <n.yasuhito@gmail.com>
2020-02-23 15:36:19 +0900

    1

---
+++ sample.php
@@ -0,0 +1,32 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+<title>PHPテスト</title>
+</head>
+<body>
+
+<h1>PHPのテストです</h1>
+
+<p>
+今日の日付は
+<?php
+echo date('Y年m月d日');
+?>
+です。
+</p>
+<form action="action.php" method="post">
+ 名前: <input type="text" name="name" />
+ 年齢: <input type="text" name="age" />
+ <input type="submit" />
+</form>
+
+
+
+
+
+</body>
+</html>
